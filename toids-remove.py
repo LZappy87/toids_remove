6fbe4a195b35d07bcbf292e20704961ea62b0123
LZappy87 <47757757+LZappy87@users.noreply.github.com>
2022-02-04 22:45:10 +0100

    Fixed some typo

--- toids-remove.py
+++ toids-remove.py
@@ -18,23 +18,25 @@
 # the script is fully configurable through the keys.py file wich contains:
 # misp_url 						| URL of the MISP instance
 # misp_key 						| The API key needed to access the Rest API
-# misp_verifycert				| If the MISP instance have a certificate (default: false)
-# misp_client_cert				| The path of the certificate (default: none)
-# misp_excluded_tags			| List of tags to exclude (need to be a list)
+# misp_verifycert					| If the MISP instance have a certificate (default: false)
+# misp_client_cert					| The path of the certificate (default: none)
+# misp_excluded_tags					| List of tags to exclude (need to be a list)
 # maxtime						| Maximum time for the misp.search (in days, ex: 365d)
 # mintime						| Minimum time for the misp.search (in days, ex: 365d)
-# Used this as the base for the script https://www.vanimpe.eu/2019/09/24/tracking-false-positives-and-disabling-to_ids-in-misp/
+# 
+# An idea developed from this article: https://www.vanimpe.eu/2019/09/24/tracking-false-positives-and-disabling-to_ids-in-misp/
 ################################
 # /!\ USE THE BELOW TEMPLATE TO GENERATE THE "keys.py" FILE /!\
 ################################
 # #!/usr/bin/env python
 #
-# Here the informations needed to connect to the REST API of MISP
+# Here the informations needed to connect to the MISP Rest API
 # misp_url = '<MISP URL HERE>'
 # misp_key = '<MISP API KEY HERE>'
 #
 #
-# If MISP have a self-signed certificate keep it to false, otherwise set it to true 
+# If the MISP instance doesn't have a self-signed certificate keep it to false, otherwise set it to true 
+# and insert the path to the certificate on 'misp_client_cert'
 # misp_verifycert = False
 # misp_client_cert = None
 #
@@ -53,23 +55,26 @@
 # - Moved misp_client_cert to keys.py;
 # - Added: misp_excluded_tags (for tag exclusion), mintime and maxtime (for time reference regarding the query on MISP) on keys.py;
 # - Added various, basilar, error handling;
-# - Added the creation of a default 'keys.py' if not present
+# - Added the creation of a default 'keys.py' if not present.
 ################################
 # TODO:
 # - More configuration parameters i guess;
+# - VTotal API implementation (for future functions);
 # - Better error handling.
 ################################
 ################################
-# Importing MISP library
+# Importing MISP library method ExpandedPyMISP
 from pymisp import ExpandedPyMISP
-# Needed to disable InsecureRequestWarning linked to self-signed of the MISP URL when opening a connection with the Rest API
-# Not needed if the destination MISP have a Certificate
-# https://stackoverflow.com/questions/27981545/suppress-insecurerequestwarning-unverified-https-request-is-being-made-in-pytho
+# Need urllib3 to disable InsecureRequestWarning error when connecting to the Rest API 
+# since my instance of MISP has a self-signed certificate, not needed if the destination 
+# MISP instance have a valid certificate
+# Source: https://stackoverflow.com/questions/27981545/suppress-insecurerequestwarning-unverified-https-request-is-being-made-in-pytho
 import urllib3
-# This is for the function suppress_output (too much lines with big databases)
+# Importing this librariesthe function suppress_output (too much lines with big databases)
+# and the default keys.py part
 from contextlib import contextmanager
 import sys, os
-# Need it just to remove temporary files in the script folder
+# Using this to clean temporary files from the source folder
 import shutil
 # Yes, now with execution timer®
 import time
@@ -87,43 +92,47 @@ def suppress_stdout():
         finally:
             sys.stdout = old_stdout
 
-# Counter for attributes modified and event_id list
+# Initializing the counter for the attribute and the event_id list
 i = 0
 event_id = []
 
 # Aaaand the SSL error is gone (because certificates are overrated)
+# Note: as said it's not necessary if the MISP instance has a signed certificate
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Establishing the connection with the MISP Rest API using the information imported from keys.py (with Error Handling)
+# Establishing the connection with the MISP Rest API using the informations imported from keys.py (now with Error Handling®)
 try:
-
+	# Importing settings from keys.py
 	from keys import misp_url, misp_key, misp_verifycert, misp_client_cert, misp_excluded_tags, mintime, maxtime
 	print(f'Attempting to connect to the Rest API of {misp_url}...')
+	# Command to connect to the Rest API
 	misp = ExpandedPyMISP(misp_url, misp_key, misp_verifycert, cert=misp_client_cert)
-	
+
+# First Exception Handler - ImportError
 # If the keys.py does not exist it generates it
-# Of course it needs to be populated with valid informations, duh
+# Note: Of course it needs to be populated with valid informations, duh
 except ImportError:
 
 	print('No keys.py file exists, generating a default file...')
 	y = 0
 	f = open("keys.py","w+")
 	
-	# Definition of the default keys.py
+	# Definition of the list with the default keys.py strings
 	deffile = [
 	'#!/usr/bin/env python',
-	'# Here the informations needed to connect to the REST API of MISP',
+	'# Here the informations needed to connect to the MISP Rest API',
 	'misp_url = <MISP URL HERE>',
 	'misp_key = <MISP API KEY HERE>',
 	'',
-	'# If MISP have a self-signed certificate keep it to false, otherwise set it to true',
+	'# If the MISP instance doesn\'t have a self-signed certificate keep it to false, otherwise set it to true',
+	'# and insert the path to the certificate on misp_client_cert',
 	'misp_verifycert = False',
 	'misp_client_cert = None',
 	'',
 	'# Here the other parameters',
-	'misp_exclude_tags = [<here the list of the tags to exclude>]',
-	'maxtime = <set max time for query, ex: 365d>',
-	'mintime = <set min time for query, ex: 365d>'
+	'misp_exclude_tags = [\'<here the list of the tags to exclude>\']',
+	'maxtime = <set max time for query, ex: \'365d\'>',
+	'mintime = <set min time for query, ex: \'365d\'>'
 	]
 	
 	# Writing the file and exiting	
@@ -135,29 +144,28 @@ except ImportError:
 	print('Default keys.py generated, please modify it with the needed informations, the script will now exit...')
 	quit()
 
-
+# Generic Exception Handler - Exception
 # For all the other problems, this part will be revised with more precise Error Handling
 except Exception:
 	print('There is a problem with the connection to MISP or the parameters in keys.py, the script will now exit...')
 	quit()
 
 # Searching and generating a list of the events where attributes with the information gathered from keys.py
-# Fixed the query and updated with suggestions from my colleagues
-# Generating an exclusion query (this part can AND will be expanded for more personalization)
-
 try:
 
+	# Generating an exclusion query (this part can AND will be expanded for more personalization)
 	tagslist = misp.build_complex_query(not_parameters=misp_excluded_tags)
-	# The uncommented line is for testing purpose, please use this string for production enviroment
-	# result = misp.search(controller='attributes', to_ids=True, tags=tagslist, timestamp=(maxtime, mintime))
-	result = misp.search(controller='attributes', to_ids=True, tags=tagslist)
-	
+	# Note: the commented string is for testing purpose (since i don't have a MISP database with old entries :D)
+	# result = misp.search(controller='attributes', to_ids=True, tags=tagslist)
+	result = misp.search(controller='attributes', to_ids=True, tags=tagslist, timestamp=(maxtime, mintime))
+
+# Generic Exception Handler - Exception
+# # Same here, to be revised...
 except Exception:
-	# Same here, to be revised...
 	print('Check if all the informations needed are into the keys.py file, the script will now exit...')
 	quit()
 
-# Disabling IDS attributes
+# From here starts the IDS tag disable part
 print('Removing IDS tags...')
 
 # Timer starts
@@ -184,7 +192,7 @@ print('###############')
 
 # Republishing all the modified events in result (if present)
 if len(event_id) > 0:
-	print(f'Done, {len(event_id)} events republished!')
+	print(f'Republish done on {len(event_id)} events!')
 else:
 	print('No need to republish events, no entry modified.')
 

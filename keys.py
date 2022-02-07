253e30cbfa720e1f2ac852424189812b606eb37e
LZappy87 <47757757+LZappy87@users.noreply.github.com>
2022-02-07 23:52:30 +0100

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# MISP API Connection Parameters
+misp_url = '<MISP URL HERE>'
+misp_key = '<MISP API KEY HERE>'
+
+# If MISP have a self-signed certificate keep it to false, otherwise set it to true
+# and populate the misp_client_cert with the path to the certificate
+misp_verifycert = False
+misp_client_cert = None
+
+# MISP Search Parameters
+misp_excluded_tags = ['<PUT HERE THE TAGS TO EXCLUDE>']
+# /!\ DO NOT TOUCH typlist VALUES FOR NOW /!\
+typlist = ['ip-src','ip-dst','domain','url','hostname']
+
+# VirusTotal APIv3 + Search Parameters
+vtotal_key = '<VTOTAL API KEY HERE>'
+maltag = ['malware','malicious']
+vlist = ['Snort IP sample list','PhishLabs','OpenPhish','AlienVault','Sophos','Fortinet','Google Safebrowsing','Abusix','EmergingThreats','MalwareDomainList','Kaspersky','URLhaus','Spamhaus','NotMining','Forcepoint ThreatSeeker','Certego','ESET','ThreatHive','FraudScore']
+vtrusted = ['Fortinet','Alienvault','Sophos','Google Safebrowsing','Abusix','Kaspersky','Forcepoint ThreatSeeker','ESET']
